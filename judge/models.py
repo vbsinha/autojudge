f53c0049fe3cbbd569bea3aea0cab000d209325a
vishwakftw <cs15btech11043@iith.ac.in>
2019-04-05 21:33:04 +0530

    Add submission

--- judge/models.py
+++ judge/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from uuid import uuid4
 from functools import partial
 
 
@@ -8,6 +9,7 @@ def upload_file_to_problem(instance, filename, extension):
     This is a callable to upload a file based on the problem ID
     """
     return '/'.join(['content', instance.code, filename + extension])
+
 
 class Problem(models.Model):
     """
@@ -56,3 +58,45 @@ class Problem(models.Model):
 
     # Setter solution script [File, Nullable]
     setter_solution = models.FileField(upload_to=partial(upload_file_to_problem, filename='setter_soln'), null=True)
+
+
+class Submission(models.Model):
+    """
+    Model for a Submission.
+    """
+    # ID of Submission [Char]
+    id = uuid4().hex
+
+    # ForeignKey to Problem
+    problem = models.ForeignKey(Problem)
+
+    # ForeignKey to Person
+    participant = models.ForeignKey(Person)
+
+    # This has to be updated periodically
+    PERMISSIBLE_FILE_TYPES = (
+                                ('.none', 'NOT_SELECTED'),
+                                ('.py', 'PYTHON'),
+                             )
+
+    # File Type [Char]
+    file_type = models.CharField(max_length=5, choices=PERMISSIBLE_FILE_TYPES, default='.none')
+
+    # Submission file [File]
+    submission_file = models.FileField(upload_to=
+                                       'content/submissions/submission_{}{}'.format(id, file_type))
+
+    # Timestamp of submission [Time]
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Judge score [Int]
+    judge_score = models.PositiveSmallIntegerField(default=0)
+
+    # TA score [Int]
+    ta_score = models.PositiveSmallIntegerField(default=0)
+
+    # Final score [Int]
+    final_score = models.FloatField(default=0.0)
+
+    # Linter score [Int, Nullable]
+    linter_score = models.FloatField(default=0.0)
