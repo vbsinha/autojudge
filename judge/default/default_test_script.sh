06eb492afb9d2f489ff6e050a2b1dec503587be7
vishwakftw <cs15btech11043@iith.ac.in>
2019-04-08 23:49:24 +0530

    Add memory and time limit checks, Dockerfile image no more minimal
    We will be using runsolver.
    
    Comments about usage are specified in the test_script
    
    Dockerfile has been modified to use a fat version of python:3

--- judge/default/default_test_script.sh
+++ judge/default/default_test_script.sh
@@ -24,24 +24,47 @@ run_submission() {
   SID=$1
   TID=$2
   TLIMIT=$3
+  MLIMIT=$4
 
-  timeout ${TLIMIT} ${SUB_FDR}/submission_${SID} < ${TEST_FDR}/inputfile_${TID}.txt > ${TMP}/sub_output_${SID}_${TID}.txt
+  # First two lines specify the flags required
+  # -w /dev/null pipes output of the tool to /dev/null, --vsize-limit gives the virtual size limit
+  # --cores 0 limits to only one core, --wall-clock-limit handles the time limit
+  # --var provides a file with specific flags which are used for checking
+  # The last line runs the process
+  timer_tool -w /dev/null --vsize-limit $MLIMIT --cores 0 --wall-clock-limit $TLIMIT \
+             --var ${TMP}/submission_status_${SID}_${TID}.txt \
+             ${SUB_FDR}/submission_${SID} < ${TEST_FDR}/inputfile_${TID}.txt > ${TMP}/sub_output_${SID}_${TID}.txt
 
-  case "$?" in
-    "0")
-        validate_submission_output ${TEST_FDR}/outputfile_${TID}.txt ${TMP}/sub_output_${SID}_${TID}.txt
-        return $?
-        ;;
-    "1")
-        return $RE
-        ;;
-    "124")
-        return $TLE
-        ;;
-    *)
-        return $NA
-        ;;
-  esac
+  # Make all the flags as env vars for checking and remove this file
+  . ${TMP}/submission_status_${SID}_${TID}.txt
+  rm ${TMP}/submission_status_${SID}_${TID}.txt
+
+  # This is what we do:
+  # - Run it with the timer_tool, and then check if the limits are maintained
+  #     - If no, return the appropriate errors
+  #     - If yes, re-run again to get the final submission output
+  #       This is then checked normally using a diff
+  if [ "$TIMEOUT" = true ] ; then
+    return $TLE
+  elif [ "$MEMOUT" = true ] ; then
+    return $OOM
+  else
+    clean_generated_output ${SID} ${TID}  # Delete the generated file to prevent any mismatch
+    ${SUB_FDR}/submission_${SID} < ${TEST_FDR}/inputfile_${TID}.txt > ${TMP}/sub_output_${SID}_${TID}.txt
+
+    case "$?" in
+      "0")
+          validate_submission_output ${TEST_FDR}/outputfile_${TID}.txt ${TMP}/sub_output_${SID}_${TID}.txt
+          return $?
+          ;;
+      "1")
+          return $RE
+          ;;
+      *)
+          return $NA
+          ;;
+    esac
+  fi
 }
 
 clean_generated_output() {
@@ -124,14 +147,17 @@ TMP="tmp"
 SUB_ID=$1
 shift
 
-# To be set from the env
+# To be set from the env, in seconds
 TIMELIMIT=1
+
+# To be set from the env, in MB
+MEMLIMIT=1
 
 # Iterate over all testcase IDs passed as command line arguments
 for TESTCASE_ID in "$@";
   do
     # Run the submission using run_submission
-    run_submission ${SUB_ID} ${TESTCASE_ID} ${TIMELIMIT}
+    run_submission ${SUB_ID} ${TESTCASE_ID} ${TIMELIMIT} ${MEMLIMIT}
 
     # The return value of the function will determine what goes into the submission status
     error_code_to_string $? ${TESTCASE_ID} >> submission_${SUB_ID}_status.txt
