2dbccd922471e5203d87a6bb7805075d584b61a4
Prateek Kumar <prateek@prateekkumar.in>
2019-04-08 22:54:42 +0530

    Set up Google OAuth2
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- judge/urls.py
+++ judge/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
+app_name = 'judge'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
