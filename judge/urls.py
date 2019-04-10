d6ed299fe0a4a7da66ff56bd8a01ee757262381a
Prateek Kumar <prateek@prateekkumar.in>
2019-04-11 01:30:41 +0530

    Google OAuth2, Basic Homepage, Create contest and problem pages (#9)
    * Basic homepage
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>
    
    * Set up Google OAuth2
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>
    
    * Create base template that can be used on other templates
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>
    
    * Implement create contest form
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>
    
    * Add new problem form page, fix datepicker in new contest page
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>
    
    * Add new problem form
    
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- judge/urls.py
+++ judge/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
+app_name = 'judge'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('contest/new/', views.new_contest, name='new_contest'),
+    path('contest/<int:contest_id>/', views.contest_detail, name='contest_detail'),
+    path('contest/<int:contest_id>/problem/new/', views.new_problem, name='new_problem'),
 ]
--- pdpjudge/settings.py
+++ pdpjudge/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -121,3 +124,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentications
+
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_URL = 'judge:login'
+LOGOUT_URL = 'judge:logout'
+LOGIN_REDIRECT_URL = 'judge:index'
+LOGOUT_REDIRECT_URL = 'judge:index'
+SOCIAL_AUTH_URL_NAMESPACE = 'judge:social'
