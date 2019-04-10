48895a155b6928f9c7d5298855ef6faf255498f4
Prateek Kumar <prateek@prateekkumar.in>
2019-04-10 23:29:54 +0530

    Add new problem form page, fix datepicker in new contest page
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- judge/views.py
+++ judge/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import render
 
+from .models import Contest
+
 # Create your views here.
 
 
@@ -13,6 +15,8 @@ def index(request):
 def new_contest(request):
     if request.method == 'POST':
         # TODO
+        contest = Contest(name=request.POST['name'], start_date='', end_date='', penalty='')
+        contest.save()
         context = {}
         return render(request, 'judge/new_contest.html', context)
     else:
