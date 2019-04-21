9a06e525e2a326b326f13cc52dffd57333c0e71d
Prateek Kumar <prateek@prateekkumar.in>
2019-04-22 01:24:28 +0530

    Refactor urls, display add person button only for posters
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- judge/views.py
+++ judge/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from datetime import timedelta
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 import logging
@@ -63,7 +63,7 @@ def new_contest(request):
                     contest_name, contest_start, contest_end, penalty, is_public)
                 if status:
                     handler.add_person_to_contest(user.email, msg, True)
-                    return redirect('/judge/')
+                    return redirect(reverse('judge:index'))
                 else:
                     logging.debug(msg)
                     form.add_error(None, 'Contest could not be created.')
@@ -106,6 +106,7 @@ def get_posters(request, contest_id, role=True):
         context['persons'] = value
     else:
         return handler404(request)
+    context['permission'] = perm
     return render(request, 'judge/contest_persons.html', context)
 
 
@@ -128,8 +129,8 @@ def add_poster(request, contest_id, role=True):
             status, err = handler.add_person_to_contest(
                 request.POST.get('email'), contest_id, role)
             if status:
-                return redirect('/judge/contest/{}/{}s/'.format(
-                    contest_id, context['type'].lower()))
+                return redirect(reverse('judge:get_{}s'.format(context['type'].lower()),
+                                        args=(contest_id,)))
             else:
                 form.non_field_errors = err
     else:
@@ -195,7 +196,7 @@ def new_problem(request, contest_id):
                 form.cleaned_data['max_score'], form.cleaned_data['compilation_script'],
                 form.cleaned_data['testing_script'], form.cleaned_data['setter_soln'])
             if status:
-                return redirect('/judge/problem/{}/'.format(code))
+                return redirect(reverse('judge:problem_detail', args=(code,)))
             else:
                 form.add_error(None, err)
     else:
@@ -221,7 +222,7 @@ def edit_problem(request, problem_id):
                 form.cleaned_data['input_format'], form.cleaned_data['output_format'],
                 form.cleaned_data['difficulty'])
             if status:
-                return redirect('/judge/problem/{}/'.format(problem.code))
+                return redirect(reverse('judge:problem_detail', args=(problem.code,)))
             else:
                 form.add_error(None, err)
     else:
@@ -254,6 +255,7 @@ def add_test_case_problem(request, problem_id):
         print('Not POST')
         return redirect(request.META['HTTP_REFERER'])
 
+
 def problem_submit(request, problem_id):
     if request.method == 'POST':
         # TODO What is file_type?
@@ -262,9 +264,9 @@ def problem_submit(request, problem_id):
             problem_id, request.user.email, '.cpp', request.FILES.get('file'), timezone.now())
         if status:
             # TODO give status
-            return redirect('/judge/')
+            return redirect(reverse('judge:index'))
         else:
             print(err)
             return redirect(request.META['HTTP_REFERER'])
     else:
-        return redirect('/judge/')
+        return redirect(reverse('judge:index'))
