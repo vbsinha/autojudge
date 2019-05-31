93cfcbe372bdc07b6231ad4ca8871730a6bbf74d
vishwakftw <cs15btech11043@iith.ac.in>
2019-05-31 15:54:03 +0530

    Add support for Go and C properly

--- judge/default/compilation_script.sh
+++ judge/default/compilation_script.sh
@@ -1,15 +1,27 @@
 # Enter the compilation script here.
 # For every language, write a function named "compile_<extension>()"
+# SUBPATH is the path of the file that has been submitted
+# EXPATH is the path for the executable generated
 
 # All required error codes
 SUCCESS=0
 FAILURE=1
 
+# This is the function to compile C files using gcc
+compile_c() {
+  SUBPATH=$1
+  EXPATH=$2
+  if gcc $SUBPATH -lm -o $EXPATH ; then
+    return $SUCCESS
+  else
+    return $FAILURE
+}
+
 # This is the function to compile CPP files using g++
 compile_cpp() {
   SUBPATH=$1
   EXPATH=$2
-  if g++ $SUBPATH -o $EXPATH ; then
+  if g++ $SUBPATH -lm -o $EXPATH ; then
     return $SUCCESS
   else
     return $FAILURE
@@ -23,4 +35,15 @@ compile_py() {
   echo "#!/usr/local/bin/python" > $EXPATH
   cat $SUBPATH >> $EXPATH
   return $SUCCESS
+}
+
+# This is the function to compile go files using go build
+compile_go() {
+  SUBPATH=$1
+  EXPATH=$2
+  if go build $SUBPATH -o $EXPATH ; then
+    return $SUCCESS
+  else
+    return $FAILURE
+  fi
 }
