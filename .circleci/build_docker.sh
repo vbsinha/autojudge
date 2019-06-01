a36b39e089af9c5420960ca5042bbcf1938e542b
vishwakftw <cs15btech11043@iith.ac.in>
2019-06-01 20:54:34 +0530

    Fix apt-transport install

--- .circleci/build_docker.sh
+++ .circleci/build_docker.sh
@@ -2,7 +2,7 @@
 
 # Setup repository
 sudo apt-get update
-sudo apt-get install -y apt-transport ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
