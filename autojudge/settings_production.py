efe7461492eeeae83919fa41d4fcaa6c2dd17ed6
Prateek Kumar <prateek@prateekkumar.in>
2019-06-02 12:28:50 +0530

    Add comments on how to change settings
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- autojudge/settings_production.py
+++ autojudge/settings_production.py
@@ -1,3 +1,6 @@
+# To use these settings during development, run with
+# python manage.py runserver --settings=autojudge.settings_production
+
 import os
 from typing import List
 from .settings import *
@@ -5,7 +8,9 @@ from .settings import *
 SECRET_KEY = os.environ.get('AUTOJUDGE_SECRET_KEY')
 DEBUG = False
 
-# Edit this in production
+# TODO Edit the url
 ALLOWED_HOSTS: List[str] = ['autojudge.iith.ac.in']
 
-# TODO Confiure Postgreql as database
+# TODO Confiure Postgrsql as database
+
+# TODO Configure static root and static url
